f59d28aaf2041eb8cebd1490e53306553025ac45
Alex <pavloveone@192.168.1.6>
2022-02-20 17:50:46 +0300

    123

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,29 +1,13 @@
-const initialCards = [                                                            //массив карточек
-    {
-        name: 'Архыз',
-        link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
-    },
-    {
-        name: 'Челябинская область',
-        link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
-    },
-    {
-        name: 'Иваново',
-        link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'
-    },
-    {
-        name: 'Камчатка',
-        link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
-    },
-    {
-        name: 'Холмогорский район',
-        link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
-    },
-    {
-        name: 'Байкал',
-        link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
-    }
-];
+const cardsContainer = document.querySelector('.elements');
+const openPopupProfile = document.querySelector('.profile__edit-button');
+const popupProfile = document.querySelector('#popup-profile');
+const openPopupCard = document.querySelector('.profile__add-button');
+const popupCard = document.querySelector('#popup-card');
+const popupProfileForm = popupProfile.querySelector('.popup');
+const nameInput = popupProfileForm.querySelector('input[name=name]');
+const jobInput = popupProfileForm.querySelector('input[name=job]');
+const avatar = document.querySelector('.profile__avatar');
+
 
 const enableValidation = ({
     formSelector: '.popup',
@@ -34,17 +18,7 @@ const enableValidation = ({
     inputErrorClass: 'popup__error_visible'
 });
 
-const cardsContainer = document.querySelector('.elements');  // поиск элементов
-const openPopupProfile = document.querySelector('.profile__edit-button');
-const popupProfile = document.querySelector('#popup-profile');
-const openPopupCard = document.querySelector('.profile__add-button');
-const popupCard = document.querySelector('#popup-card');
-const popupProfileForm = popupProfile.querySelector('.popup');
-const nameInput = popupProfileForm.querySelector('input[name=name]');
-const jobInput = popupProfileForm.querySelector('input[name=job]');
-
 export { 
-    initialCards,
     enableValidation,
     cardsContainer,
     openPopupProfile,
@@ -53,5 +27,6 @@ export {
     popupCard,
     jobInput,
     nameInput,
-    popupProfileForm
+    popupProfileForm,
+    avatar
 };
